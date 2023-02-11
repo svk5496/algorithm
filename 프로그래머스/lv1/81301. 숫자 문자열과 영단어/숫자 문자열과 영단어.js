a00957491295c48d23cc2604f43928d0553dd47c
Sungho Kim <54943533+svk5496@users.noreply.github.com>
2023-02-11 14:46:31 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    var answer = 0;
+    answer = s
+            .replace(/one/g,"1")
+            .replace(/two/g,"2")
+            .replace(/three/g,"3")
+            .replace(/four/g,"4")
+            .replace(/five/g,"5")
+            .replace(/six/g,"6")
+            .replace(/seven/g,"7")
+            .replace(/eight/g,"8")
+            .replace(/nine/g,"9")
+            .replace(/zero/g,"0")
+            
+    return answer/1;
+}
